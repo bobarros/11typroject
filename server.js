f097daf01ef60597c40fc25a88f31e3a2322e038
Bruno Barros <bobarros@gmail.com>
2021-03-03 10:46:36 -0300

    adding compression

--- server.js
+++ server.js
@@ -3,6 +3,15 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const compression = require("compression");
+
+app.use(compression({ filter: shouldCompress }));
+function shouldCompress(req, res) {
+  if (req.headers["x-no-compression"]) {
+    return false;
+  }
+  return compression.filter(req, res);
+}
 
 app.use(express.static("./_site"));
 
@@ -15,5 +24,5 @@ app.get("*", function (req, res) {
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Hosted at port: ' + (process.env.PORT || 3000))
+  console.log("Hosted at port: " + (process.env.PORT || 3000));
 });
